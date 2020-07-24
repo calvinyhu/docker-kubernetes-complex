3996d14b1cb43d763e26213ec859c3d606863a81
Calvin Hu <ycalvinhu@gmail.com>
2020-07-23 22:35:32 -0700

    add docker-compose

--- server/index.js
+++ server/index.js
@@ -21,7 +21,9 @@ const pgClient = new Pool({
   port: keys.pgPort,
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch((err) => console.log(err));
+pgClient.on('connect', () => {
+  pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch((err) => console.log(err));
+});
 
 // Redis client setup
 const redisClient = redis.createClient({
