2612071d65a7bb85fa6bd4c52f80cf0eacf24a47
Calvin Hu <ycalvinhu@gmail.com>
2020-07-25 20:07:20 -0700

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ const App = () => (
   <Router>
     <div>
       <header>
+        <h1>Fib Calculator</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       </header>
